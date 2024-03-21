fac98ca89dde2f9833a65322aa092e0893b2092a
tongari07 <togawa.naoto@moneyforward.co.jp>
2024-03-21 13:04:18 +0900

    fix

--- keyboards/omkbd/ergodash/rev1/keymaps/tongari07/config.h
+++ keyboards/omkbd/ergodash/rev1/keymaps/tongari07/config.h
@@ -25,3 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 150
